bcfeed3beb709990f1cbb41358db79c787bf1671
Tim Gudlewski <t.gudlewski@gmail.com>
2023-04-22 19:16:07 -0400

    Start the move/copy functionality

--- p3_lib.c
+++ p3_lib.c
@@ -1,7 +1,10 @@
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
 void lib_test() { printf("hello from lib\n"); }
 
 int called_as_copy(char prog_name[]) {
@@ -32,4 +35,26 @@ int get_inode_num(char *path, struct stat *sb) {
     return sb->st_ino;
   }
   return 0;
+}
+
+void move_file(char *src, char *dst) {
+  if (link(src, dst) < 0) {
+    fprintf(stdout, "Can't link to destination %s\n", dst);
+    perror("link");
+  }
+}
+
+void copy_file(char *src, char *dst) {
+  int src_fd = open(src, O_RDONLY);
+  int dst_fd = open(dst, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  if (src_fd < 0 || dst_fd < 0) {
+    perror("open");
+    exit(EXIT_FAILURE);
+  }
+  char buf[BUFSIZ];
+  if (read(src_fd, buf, BUFSIZ) == -1) {
+    perror("read");
+    exit(EXIT_FAILURE);
+  }
+  char c;
 }
--- p3_lib.h
+++ p3_lib.h
@@ -6,3 +6,5 @@ int is_reg_file(char *path, struct stat *sb);
 int is_dir(char *path, struct stat *sb);
 int get_inode_num(char *path, struct stat *sb);
 int file_exists(char *path);
+void move_file(char *src, char *dst);
+void copy_file(char *src, char *dst);
